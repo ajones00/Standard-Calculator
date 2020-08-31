107ef732b9157983ad3ad78385da9539c7211297
Anthony Jones <jonesant0919@gmail.com>
2020-08-31 17:55:18 -0400

    Changed ui color

---
+++ Calculator/Form1.Designer.vb
@@ -0,0 +1,313 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.equalsButton = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.clearEntryButton = New System.Windows.Forms.Button()
+        Me.Clear = New System.Windows.Forms.Button()
+        Me.backspaceButton = New System.Windows.Forms.Button()
+        Me.divideButton = New System.Windows.Forms.Button()
+        Me.button9 = New System.Windows.Forms.Button()
+        Me.button8 = New System.Windows.Forms.Button()
+        Me.button7 = New System.Windows.Forms.Button()
+        Me.multiplyButton = New System.Windows.Forms.Button()
+        Me.button6 = New System.Windows.Forms.Button()
+        Me.button5 = New System.Windows.Forms.Button()
+        Me.button4 = New System.Windows.Forms.Button()
+        Me.subtractButton = New System.Windows.Forms.Button()
+        Me.button3 = New System.Windows.Forms.Button()
+        Me.button2 = New System.Windows.Forms.Button()
+        Me.button1 = New System.Windows.Forms.Button()
+        Me.addButton = New System.Windows.Forms.Button()
+        Me.decimalButton = New System.Windows.Forms.Button()
+        Me.button0 = New System.Windows.Forms.Button()
+        Me.posNegButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'equalsButton
+        '
+        Me.equalsButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.equalsButton.Location = New System.Drawing.Point(12, 67)
+        Me.equalsButton.Name = "equalsButton"
+        Me.equalsButton.Size = New System.Drawing.Size(100, 78)
+        Me.equalsButton.TabIndex = 0
+        Me.equalsButton.Text = "="
+        Me.equalsButton.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(12, 12)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(418, 49)
+        Me.TextBox1.TabIndex = 1
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'clearEntryButton
+        '
+        Me.clearEntryButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.clearEntryButton.Location = New System.Drawing.Point(118, 67)
+        Me.clearEntryButton.Name = "clearEntryButton"
+        Me.clearEntryButton.Size = New System.Drawing.Size(100, 78)
+        Me.clearEntryButton.TabIndex = 2
+        Me.clearEntryButton.Text = "CE"
+        Me.clearEntryButton.UseVisualStyleBackColor = True
+        '
+        'Clear
+        '
+        Me.Clear.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Clear.Location = New System.Drawing.Point(224, 67)
+        Me.Clear.Name = "Clear"
+        Me.Clear.Size = New System.Drawing.Size(100, 78)
+        Me.Clear.TabIndex = 3
+        Me.Clear.Text = "C"
+        Me.Clear.UseVisualStyleBackColor = True
+        '
+        'backspaceButton
+        '
+        Me.backspaceButton.Font = New System.Drawing.Font("Wingdings", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(2, Byte))
+        Me.backspaceButton.Location = New System.Drawing.Point(330, 67)
+        Me.backspaceButton.Name = "backspaceButton"
+        Me.backspaceButton.Size = New System.Drawing.Size(100, 78)
+        Me.backspaceButton.TabIndex = 4
+        Me.backspaceButton.Text = ""
+        Me.backspaceButton.UseVisualStyleBackColor = True
+        '
+        'divideButton
+        '
+        Me.divideButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.divideButton.Location = New System.Drawing.Point(330, 151)
+        Me.divideButton.Name = "divideButton"
+        Me.divideButton.Size = New System.Drawing.Size(100, 78)
+        Me.divideButton.TabIndex = 8
+        Me.divideButton.Text = "/"
+        Me.divideButton.UseVisualStyleBackColor = True
+        '
+        'button9
+        '
+        Me.button9.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.button9.Location = New System.Drawing.Point(224, 151)
+        Me.button9.Name = "button9"
+        Me.button9.Size = New System.Drawing.Size(100, 78)
+        Me.button9.TabIndex = 7
+        Me.button9.Text = "9"
+        Me.button9.UseVisualStyleBackColor = True
+        '
+        'button8
+        '
+        Me.button8.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.button8.Location = New System.Drawing.Point(118, 151)
+        Me.button8.Name = "button8"
+        Me.button8.Size = New System.Drawing.Size(100, 78)
+        Me.button8.TabIndex = 6
+        Me.button8.Text = "8"
+        Me.button8.UseVisualStyleBackColor = True
+        '
+        'button7
+        '
+        Me.button7.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.button7.Location = New System.Drawing.Point(12, 151)
+        Me.button7.Name = "button7"
+        Me.button7.Size = New System.Drawing.Size(100, 78)
+        Me.button7.TabIndex = 5
+        Me.button7.Text = "7"
+        Me.button7.UseVisualStyleBackColor = True
+        '
+        'multiplyButton
+        '
+        Me.multiplyButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.multiplyButton.Location = New System.Drawing.Point(330, 235)
+        Me.multiplyButton.Name = "multiplyButton"
+        Me.multiplyButton.Size = New System.Drawing.Size(100, 78)
+        Me.multiplyButton.TabIndex = 12
+        Me.multiplyButton.Text = "x"
+        Me.multiplyButton.UseVisualStyleBackColor = True
+        '
+        'button6
+        '
+        Me.button6.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.button6.Location = New System.Drawing.Point(224, 235)
+        Me.button6.Name = "button6"
+        Me.button6.Size = New System.Drawing.Size(100, 78)
+        Me.button6.TabIndex = 11
+        Me.button6.Text = "6"
+        Me.button6.UseVisualStyleBackColor = True
+        '
+        'button5
+        '
+        Me.button5.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.button5.Location = New System.Drawing.Point(118, 235)
+        Me.button5.Name = "button5"
+        Me.button5.Size = New System.Drawing.Size(100, 78)
+        Me.button5.TabIndex = 10
+        Me.button5.Text = "5"
+        Me.button5.UseVisualStyleBackColor = True
+        '
+        'button4
+        '
+        Me.button4.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.button4.Location = New System.Drawing.Point(12, 235)
+        Me.button4.Name = "button4"
+        Me.button4.Size = New System.Drawing.Size(100, 78)
+        Me.button4.TabIndex = 9
+        Me.button4.Text = "4"
+        Me.button4.UseVisualStyleBackColor = True
+        '
+        'subtractButton
+        '
+        Me.subtractButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.subtractButton.Location = New System.Drawing.Point(330, 319)
+        Me.subtractButton.Name = "subtractButton"
+        Me.subtractButton.Size = New System.Drawing.Size(100, 78)
+        Me.subtractButton.TabIndex = 16
+        Me.subtractButton.Text = "-"
+        Me.subtractButton.UseVisualStyleBackColor = True
+        '
+        'button3
+        '
+        Me.button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.button3.Location = New System.Drawing.Point(224, 319)
+        Me.button3.Name = "button3"
+        Me.button3.Size = New System.Drawing.Size(100, 78)
+        Me.button3.TabIndex = 15
+        Me.button3.Text = "3"
+        Me.button3.UseVisualStyleBackColor = True
+        '
+        'button2
+        '
+        Me.button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.button2.Location = New System.Drawing.Point(118, 319)
+        Me.button2.Name = "button2"
+        Me.button2.Size = New System.Drawing.Size(100, 78)
+        Me.button2.TabIndex = 14
+        Me.button2.Text = "2"
+        Me.button2.UseVisualStyleBackColor = True
+        '
+        'button1
+        '
+        Me.button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.button1.Location = New System.Drawing.Point(12, 319)
+        Me.button1.Name = "button1"
+        Me.button1.Size = New System.Drawing.Size(100, 78)
+        Me.button1.TabIndex = 13
+        Me.button1.Text = "1"
+        Me.button1.UseVisualStyleBackColor = True
+        '
+        'addButton
+        '
+        Me.addButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.addButton.Location = New System.Drawing.Point(330, 403)
+        Me.addButton.Name = "addButton"
+        Me.addButton.Size = New System.Drawing.Size(100, 78)
+        Me.addButton.TabIndex = 20
+        Me.addButton.Text = "+"
+        Me.addButton.UseVisualStyleBackColor = True
+        '
+        'decimalButton
+        '
+        Me.decimalButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.decimalButton.Location = New System.Drawing.Point(224, 403)
+        Me.decimalButton.Name = "decimalButton"
+        Me.decimalButton.Size = New System.Drawing.Size(100, 78)
+        Me.decimalButton.TabIndex = 19
+        Me.decimalButton.Text = "."
+        Me.decimalButton.UseVisualStyleBackColor = True
+        '
+        'button0
+        '
+        Me.button0.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.button0.Location = New System.Drawing.Point(118, 403)
+        Me.button0.Name = "button0"
+        Me.button0.Size = New System.Drawing.Size(100, 78)
+        Me.button0.TabIndex = 18
+        Me.button0.Text = "0"
+        Me.button0.UseVisualStyleBackColor = True
+        '
+        'posNegButton
+        '
+        Me.posNegButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.posNegButton.Location = New System.Drawing.Point(12, 403)
+        Me.posNegButton.Name = "posNegButton"
+        Me.posNegButton.Size = New System.Drawing.Size(100, 78)
+        Me.posNegButton.TabIndex = 17
+        Me.posNegButton.Text = "+/-"
+        Me.posNegButton.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Control
+        Me.ClientSize = New System.Drawing.Size(436, 489)
+        Me.Controls.Add(Me.addButton)
+        Me.Controls.Add(Me.decimalButton)
+        Me.Controls.Add(Me.button0)
+        Me.Controls.Add(Me.posNegButton)
+        Me.Controls.Add(Me.subtractButton)
+        Me.Controls.Add(Me.button3)
+        Me.Controls.Add(Me.button2)
+        Me.Controls.Add(Me.button1)
+        Me.Controls.Add(Me.multiplyButton)
+        Me.Controls.Add(Me.button6)
+        Me.Controls.Add(Me.button5)
+        Me.Controls.Add(Me.button4)
+        Me.Controls.Add(Me.divideButton)
+        Me.Controls.Add(Me.button9)
+        Me.Controls.Add(Me.button8)
+        Me.Controls.Add(Me.button7)
+        Me.Controls.Add(Me.backspaceButton)
+        Me.Controls.Add(Me.Clear)
+        Me.Controls.Add(Me.clearEntryButton)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.equalsButton)
+        Me.Name = "Form1"
+        Me.Text = "Standard Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents equalsButton As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents clearEntryButton As Button
+    Friend WithEvents Clear As Button
+    Friend WithEvents backspaceButton As Button
+    Friend WithEvents divideButton As Button
+    Friend WithEvents button9 As Button
+    Friend WithEvents button8 As Button
+    Friend WithEvents button7 As Button
+    Friend WithEvents multiplyButton As Button
+    Friend WithEvents button6 As Button
+    Friend WithEvents button5 As Button
+    Friend WithEvents button4 As Button
+    Friend WithEvents subtractButton As Button
+    Friend WithEvents button3 As Button
+    Friend WithEvents button2 As Button
+    Friend WithEvents button1 As Button
+    Friend WithEvents addButton As Button
+    Friend WithEvents decimalButton As Button
+    Friend WithEvents button0 As Button
+    Friend WithEvents posNegButton As Button
+End Class
---
+++ Calculator/Form1.vb
@@ -0,0 +1,63 @@
+﻿Public Class Form1
+    'variables to hold data
+    Dim firstNumber, secondNumber, result As Double
+    Dim operate As String
+
+    Private Sub Numbers_Click(sender As Object, e As EventArgs) Handles decimalButton.Click, button9.Click, button8.Click, button7.Click, button6.Click, button5.Click, button4.Click, button3.Click, button2.Click, button1.Click, button0.Click
+        Dim holder As Button = sender
+        If (TextBox1.Text = "0") Then
+            TextBox1.Text = ""
+            TextBox1.Text = holder.Text
+        ElseIf (holder.Text = ".") Then
+            If (Not TextBox1.Text.Contains(".")) Then
+                TextBox1.Text = TextBox1.Text + holder.Text
+            End If
+        Else
+            TextBox1.Text = TextBox1.Text + holder.Text
+        End If
+    End Sub
+
+    Private Sub EqualsButton_Click(sender As Object, e As EventArgs) Handles equalsButton.Click
+        secondNumber = TextBox1.Text
+        If (operate = "/") Then
+            result = firstNumber / secondNumber
+            TextBox1.Text = result
+        ElseIf (operate = "x") Then
+            result = firstNumber * secondNumber
+            TextBox1.Text = result
+        ElseIf (operate = "-") Then
+            result = firstNumber - secondNumber
+            TextBox1.Text = result
+        ElseIf (operate = "+") Then
+            result = firstNumber + secondNumber
+            TextBox1.Text = result
+        End If
+    End Sub
+
+    Private Sub PosNegButton_Click(sender As Object, e As EventArgs) Handles posNegButton.Click
+        If (TextBox1.Text.Contains("-")) Then
+            TextBox1.Text = TextBox1.Text.Remove(0, 1)
+        Else
+            TextBox1.Text = "-" + TextBox1.Text
+        End If
+    End Sub
+
+    Private Sub BackspaceButton_Click(sender As Object, e As EventArgs) Handles backspaceButton.Click
+        If (TextBox1.Text.Length > 0) Then
+            TextBox1.Text = TextBox1.Text.Remove(TextBox1.Text.Length - 1, 1)
+        End If
+        If (TextBox1.Text = "") Then
+            TextBox1.Text = "0"
+        End If
+        If (TextBox1.Text = ".") Then
+            TextBox1.Text = TextBox1.Text.Remove(TextBox1.Text.Length - 1, 1)
+        End If
+    End Sub
+
+    Private Sub Operator_Click(sender As Object, e As EventArgs) Handles subtractButton.Click, multiplyButton.Click, divideButton.Click, addButton.Click
+        Dim holder As Button = sender
+        firstNumber = TextBox1.Text
+        operate = holder.Text
+        TextBox1.Text = ""
+    End Sub
+End Class
